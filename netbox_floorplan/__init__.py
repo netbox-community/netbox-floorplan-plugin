63cbb82da83a24396fb2a84b0d8b0be38dff9122
Chris Russell <cruse1977123@gmail.com>
2026-05-25 19:36:47 +0100

    version bumps

--- netbox_floorplan/__init__.py
+++ netbox_floorplan/__init__.py
@@ -9,7 +9,7 @@ class FloorplanConfig(PluginConfig):
     description = ""
     version = __version__
     base_url = "floorplan"
-    min_version = "4.6.0-beta1"
+    min_version = "4.6.0"
     max_version = "4.6.99"
 
 
